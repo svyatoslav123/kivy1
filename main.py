1d67c2954999ce2079934acb7b986367117da72f
Lpopivnyak <136169348+LPopivnyak@users.noreply.github.com>
2024-09-03 17:22:04 +0300

    asa

--- main.py
+++ main.py
@@ -3,13 +3,31 @@ from kivy.app import App
 from kivy.uix.label import Label
 from kivy.uix.button import Button
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.window import Window
+from kivy.uix.screenmanager import ScreenManager, Screen
 
+Window.size = (800, 600)
 
 class MyApp(App):
-    txt = Label(text="виберіть екран")
-    box_button = BoxLayout()
-    box_button = BoxLayout(oreintation = "vertical",)
+    def go_to_game_selection(self, *args):
+        self.manager.current = "game_selection"
+    def __init__(self,**kwargs):
+        super(MainScreen, self).__init__(**kwargs)
+        layout = BoxLayout(orientation="vertical")
+        self.add_widget(layout)
 
+        self.title = Label(text = 'play',font_size='48sp', size_hint(1, 0.8))
+        layout.add_widget(self.title)
+
+        play_button = Button(text='play',size_hint=(1, 0.2))
+
+        play_button.bind(on_press=self.go_to)
+
+class ClickerApp(App):
+   def build(self):
+    sm = ScreenManager()
+    sm.add_widget(MainScreen(name='main'))
+    return sm
 
 
 class ScrButton(Button):
