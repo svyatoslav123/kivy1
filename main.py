eb73d548b528afe9e8d98d8cc61789068a8330b0
Lpopivnyak <136169348+LPopivnyak@users.noreply.github.com>
2024-09-10 17:23:55 +0300

    hh

--- main.py
+++ main.py
@@ -1,52 +1,67 @@
-
 from kivy.app import App
-from kivy.uix.label import Label
+from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.uix.button import Button
 from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.window import Window
-from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.uix.label import Label
+from kivy.core.window import Window
+from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.image import Image
 
+# Встановлюємо розміри вікна для зручності тестування
 Window.size = (800, 600)
 
+
+# Головне вікно
 class MainScreen(Screen):
     def go_to_game_selection(self, *args):
-        self.manager.current = "game_selection"
-    def __init__(self,**kwargs):
+        self.manager.current = 'game_selection'
+
+    def __init__(self, **kwargs):
         super(MainScreen, self).__init__(**kwargs)
-        layout = BoxLayout(orientation="vertical")
+        layout = BoxLayout(orientation='vertical')
         self.add_widget(layout)
 
-        self.title = Label(text = 'play',font_size='48sp', size_hint=(1, 0.8))
+        # Назва гри
+        self.title = Label(text='Гра', font_size='48sp', size_hint=(1, 0.8))
         layout.add_widget(self.title)
 
-        play_button = Button(text='play',size_hint=(1, 0.2))
-
-        play_button.bind(on_press=self.go_to_game_selection())
-        layout.add_widget(self.play_button)
-
-
+        # Кнопка для переходу до вибору гри
+        play_button = Button(text='Грати', size_hint=(1, 0.2))
+        play_button.bind(on_press=self.go_to_game_selection)
+        layout.add_widget(play_button)
 
 
+class GameSelectionScreen(Screen):
+    def go_to_basketball(self, *args):
+        self.manager.current = 'basketball'
 
+    def go_to_football(self, *args):
+        self.manager.current = 'football'
 
-class ScrButton(Button):
-    def __init__(self, screen ,direction = 'right', goal = 'main',**kwargs):
-        super().__init__(**kwargs)
-        self.screen = screen
-        self.direction = direction
-        self.goal = goal
+    def __init__(self, **kwargs):
+        super(GameSelectionScreen,self).__init__(**kwargs)
+        layout = BoxLayout(orientation = 'vertical')
+        self.add_widget(layout)
 
-    def on_press(self):
-        self.screen.manger.transition.direction = self.direction
-        self.screen.manger.current = self.goal
+class BasketBallScreen(Screen):
+    def __init__(self,**kwargs):
+        super(BasketBallScreen,self).__init__(**kwargs)
+        layout = FloatLayout()
+        self.add_widget(layout)
+        self.background = Image(source = ".png", size_hint=(1,1))
+        layout.add_widget(self.background)
+
+        
+
 
 class ClickerApp(App):
     def build(self):
         sm = ScreenManager()
         sm.add_widget(MainScreen(name='main'))
+        sm.add_widget(GameSelectionScreen(name='GameSelection'))
         return sm
 
 
-app = MyApp()
-app.run()
 
+if __name__ == '__main__':
+    ClickerApp().run()
