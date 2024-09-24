076043ca1740d36b309dcbd18c6c14f84ade3291
Lpopivnyak <136169348+LPopivnyak@users.noreply.github.com>
2024-09-24 17:29:24 +0300

    wwww

--- maria.py
+++ maria.py
@@ -1,18 +1,18 @@
 from kivy.app import App
 from kivy.uix.button import Button
 from kivy.uix.widget import Widget
-from kivy.uix.graphics import Ellipse, Line, Color
-from random import randint
+from kivy.graphics import Ellipse, Line, Color
+from random import random
 
 class PaintWidget(Widget):
-    def on_touch_down_ellipse(self, touch):
-        color = (randint(), 1, 1)
+    def on_touch_down(self, touch):
+        color = (random(), 1, 1)
         with self.canvas:
             Color(*color, mode = 'hsv')
             d = 30
             Ellipse(pos = (touch.x - d/2, touch.y - d/2), size=(d,d))
             touch.ud['line'] = Line (points = (touch.x, touch.y))
-    def on_touch_move_ellipse(selfself, touch):
+    def on_touch_move(self, touch):
         touch.ud['line'].points += [touch.x, touch.y]
 
 class MyPaintApp(App):
@@ -20,12 +20,12 @@ class MyPaintApp(App):
         paint = Widget()
         self.painter = PaintWidget()
         clear_button = Button(text = 'Clear')
-        clear_button.bind(on_relese = self.clear_canvas)
+        clear_button.bind(on_release = self.clear_canvas)
         paint.add_widget(self.painter)
         paint.add_widget(clear_button)
         return paint
-    def clear_canvas(self, *args):
-        self.paint. canvas.clear()
+    def clear_canvas(self, obj):
+        self.painter.canvas.clear()
 
 if __name__=='__main__':
     MyPaintApp().run()
